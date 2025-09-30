5415536696dbeb57085d4fe0b94cc8902abb2313
João Menegasso <jmenegasso@nelogica.com.br>
2025-09-30 13:24:08 -0300

    feat: include a trash icon

--- src/components/NoteBlock.vue
+++ src/components/NoteBlock.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { noteController } from '@/stores/noteManager';
+import TrashIcon from './icons/TrashIcon.vue';
 
 noteController.create('Titulo', 'Subtítulo', 'Conteúdo');
 noteController.create('Titulo', 'Subtítulo', 'Conteúdo');
@@ -10,15 +11,20 @@ const notes = noteController.view();
 console.log(notes);
 </script>
 <template>
-    <section class="flex flex-col gap-3">
-        <div v-for="note in notes" :key="note.id" class="bg-violet-500 rounded-e-lg my-3">
-            <h1>{{ note.title }}</h1>
+    <section class="flex flex-col gap-3 bg-violet-100">
+        <div
+            v-for="note in notes"
+            :key="note.id"
+            class="flex flex-col bg-violet-300 rounded-sm my-3 min-w-50 text-black"
+        >
+            <h1 class="text-lg font-bold text-center">{{ note.title }}</h1>
             <h2>{{ note.subtitle }}</h2>
+            <p>{{ note.content }}</p>
             <button
-                class="bg-red-400 text-white rounded-lg p-2 hover:bg-red-500 focus:outline-offset-2 focus:outline-red-600"
+                class="p-0.1 self-end hover:scale-110 active:scale-60"
                 @click="noteController.delete(note.id)"
             >
-                delete
+                <TrashIcon class="w-8 h-8" />
             </button>
         </div>
     </section>
