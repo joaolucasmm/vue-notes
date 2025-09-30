1795adbff76bb1a5a7282b8f694aad53b4fb0f24
João Menegasso <jmenegasso@nelogica.com.br>
2025-09-30 12:54:12 -0300

    feat: create delete function

--- src/components/NoteBlock.vue
+++ src/components/NoteBlock.vue
@@ -1,21 +1,26 @@
 <script setup lang="ts">
-import NoteController from '@/stores/noteManager'
+import { noteController } from '@/stores/noteManager';
 
-const noteController = new NoteController()
-noteController.create('Titulo', 'Subtítulo', 'Conteúdo')
-noteController.create('Titulo', 'Subtítulo', 'Conteúdo')
-noteController.create('Titulo 2', 'Subtítulo 2', 'Conteúdo 2')
-noteController.create('Titulo 3', 'Subtítulo 3', 'Conteúdo 3')
+noteController.create('Titulo', 'Subtítulo', 'Conteúdo');
+noteController.create('Titulo', 'Subtítulo', 'Conteúdo');
+noteController.create('Titulo 2', 'Subtítulo 2', 'Conteúdo 2');
+noteController.create('Titulo 3', 'Subtítulo 3', 'Conteúdo 3');
 
-const notes = noteController.view()
-console.log(notes)
+const notes = noteController.view();
+console.log(notes);
 </script>
 <template>
-  <section class="flex flex-col gap-3">
-    <div v-for="note in notes" :key="note.id" class="bg-violet-500 rounded-e-lg my-3">
-      <h1>{{ note.title }}</h1>
-      <h2>{{ note.subtitle }}</h2>
-    </div>
-  </section>
+    <section class="flex flex-col gap-3">
+        <div v-for="note in notes" :key="note.id" class="bg-violet-500 rounded-e-lg my-3">
+            <h1>{{ note.title }}</h1>
+            <h2>{{ note.subtitle }}</h2>
+            <button
+                class="bg-red-400 text-white rounded-lg p-2 hover:bg-red-500 focus:outline-offset-2 focus:outline-red-600"
+                @click="noteController.delete(note.id)"
+            >
+                delete
+            </button>
+        </div>
+    </section>
 </template>
 <style lang="scss"></style>
--- src/stores/noteManager.ts
+++ src/stores/noteManager.ts
@@ -1,20 +1,36 @@
+import { ref, type Ref } from 'vue';
+
 type Note = {
-  id: string
-  title: string
-  subtitle: string
-  content: string
-  date: number
+    id: string;
+    title: string;
+    subtitle: string;
+    content: string;
+    date: number;
+};
+
+class NoteController {
+    objects: Ref<Note[]> = ref([]);
+
+    create(title: string, subtitle: string, content: string) {
+        const date = Date.now();
+        this.objects.value.push({
+            id: 'id' + date + Math.random().toString(16).slice(2),
+            title,
+            subtitle,
+            content,
+            date: date,
+        });
+    }
+
+    delete(id: string) {
+        console.log(id);
+        this.objects.value = this.objects.value.filter((note) => note.id !== id);
+        console.log(this.objects.value);
+    }
+
+    view() {
+        return this.objects;
+    }
 }
 
-export default class NoteController {
-  objects: Note[] = []
-
-  create(title: string, subtitle: string, content: string) {
-    const date = Date.now()
-    this.objects.push({ id: 'id' + date, title, subtitle, content, date: date })
-  }
-
-  view() {
-    return this.objects
-  }
-}
+export const noteController = new NoteController();
